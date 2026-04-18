324e7ebdcccf1503f0bf3b4a424a0d9eaf87066f
Irfan Ahmad <111637705+coder404notfound@users.noreply.github.com>
2026-04-18 17:17:43 +0530

    Reverted

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ export default function App() {
   return (
     <BrowserRouter>
 
-      {/* Toast system (global) */}
+      {/* Toast system */}
       <Toaster
         position="top-right"
         toastOptions={{
